6d55493d18499628aae2099f585f72ffd13bad8e
suding <jhw02045@naver.com>
2024-03-11 18:18:29 +0900

    Create-120817: 배열의 평균값

---
+++ programmers/src/main/java/week6/suding/배열의_평균값.java
@@ -0,0 +1,16 @@
+package week6.suding;
+
+public class 배열의_평균값 {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        answer = (double)sum / numbers.length;
+        return answer;
+    }
+}
+값
