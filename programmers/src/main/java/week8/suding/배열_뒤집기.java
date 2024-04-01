c204fd24f03bf434c1a2346a0996fdc0d13f2af5
suding <jhw02045@naver.com>
2024-04-01 17:28:56 +0900

    Create-120821: 배열 뒤집기

---
+++ programmers/src/main/java/week8/suding/배열_뒤집기.java
@@ -0,0 +1,13 @@
+package week8.suding;
+
+public class 배열_뒤집기 {
+    class Solution {
+        public int[] solution(int[] num_list) {
+            int[] answer = new int[num_list.length];
+            for (int i = 0; i < num_list.length; i++) {
+                answer[i] = num_list[num_list.length - 1 - i];
+            }
+            return answer;
+        }
+    }
+}
