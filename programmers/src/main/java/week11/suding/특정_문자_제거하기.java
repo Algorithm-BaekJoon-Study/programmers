5be647f684d3b978a9d207c36952e653f5381dd6
suding <jhw02045@naver.com>
2024-05-10 16:24:20 +0900

    Create-120826: 특정 문자 제거하기

---
+++ programmers/src/main/java/week11/suding/특정_문자_제거하기.java
@@ -0,0 +1,15 @@
+package week11.suding;
+
+public class 특정_문자_제거하기 {
+    class Solution {
+        public String solution(String my_string, String letter) {
+            String answer = "";
+            for (int i = 0; i < my_string.length(); i++) {
+                if (!String.valueOf(my_string.charAt(i)).equals(letter)) {
+                    answer += my_string.charAt(i);
+                }
+            }
+            return answer;
+        }
+    }
+}
