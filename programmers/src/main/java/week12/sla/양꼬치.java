bc4ccb857d8f8823c06f568982c95b0f4ebd169c
seula123 <sla6239@gmail.com>
2024-05-14 17:32:57 +0900

    Create-120830: 양꼬치

---
+++ programmers/src/main/java/week12/sla/양꼬치.java
@@ -0,0 +1,11 @@
+package week12.sla;
+
+public class 양꼬치 {
+    public int solution(int n, int k) {
+        int answer = 0;
+
+        answer = 12000*n + 2000*(k-(n/10));
+
+        return answer;
+    }
+}
